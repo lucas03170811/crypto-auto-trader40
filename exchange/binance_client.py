56fe468a44312655609f8d00d130b7d0f7e5da90
lucas03170811 <lucas03170811@gmail.com>
2025-07-22 17:02:22 +0800

    Update binance_client.py

--- exchange/binance_client.py
+++ exchange/binance_client.py
@@ -17,7 +17,7 @@ class BinanceClient:
             testnet=TESTNET,
         )
         await self.client._request_futures_api(
-            method="POST",# ✅ 使用小寫
+            method="post",# ✅ 使用小寫
             path="positionSide/dual",
             data={"dualSidePosition": "true"},
         )
