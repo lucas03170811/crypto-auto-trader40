4b40a927286d4a20c79a22c83bc54d99db1aff5f
lucas03170811 <lucas03170811@gmail.com>
2025-07-30 17:18:51 +0800

    Update revert.py

--- strategy/revert.py
+++ strategy/revert.py
@@ -1,17 +1,13 @@
 import pandas as pd
 import pandas_ta as ta
-from decimal import Decimal
 
-def revert_signal(df: pd.DataFrame):
-    df.ta.bbands(length=20, append=True)
+def generate_revert_signal(data):
+    df = pd.DataFrame(data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'x','x','x','x','x'])
+    df['close'] = pd.to_numeric(df['close'])
     df.ta.rsi(length=14, append=True)
 
-    price = Decimal(df["close"].iloc[-1])
-    lower = df["BBL_20_2.0"].iloc[-1]
-    upper = df["BBU_20_2.0"].iloc[-1]
-    rsi   = df["RSI_14"].iloc[-1]
-
-    long  = price < Decimal(lower) and rsi < 35
-    short = price > Decimal(upper) and rsi > 65
-
-    return long, short
+    if df['RSI_14'].iloc[-1] < 30:
+        return 'LONG'
+    elif df['RSI_14'].iloc[-1] > 70:
+        return 'SHORT'
+    return None
