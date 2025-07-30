01416444a2fa7b34158220fadb327d245e9b387d
lucas03170811 <lucas03170811@gmail.com>
2025-07-30 17:56:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import asyncio
 from engine.hedge_engine import HedgeEngine
 from exchange.binance_client import BinanceClient
