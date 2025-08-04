e03d48c9c46d0dd05bd211fd13d74ddceaf9fb1b
lucas03170811 <lucas03170811@gmail.com>
2025-08-04 13:57:07 +0800

    Update trend.py

--- strategy/trend.py
+++ strategy/trend.py
@@ -2,13 +2,21 @@ import pandas as pd
 import pandas_ta as ta
 
 def generate_trend_signal(data):
-    df = pd.DataFrame(data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'x','x','x','x','x'])
-    df['close'] = pd.to_numeric(df['close'])
-    df.ta.ema(length=20, append=True)
-    df.ta.ema(length=50, append=True)
+    df = pd.DataFrame(data, columns=[
+        'timestamp', 'open', 'high', 'low', 'close', 'volume',
+        'close_time', 'quote_asset_volume', 'num_trades',
+        'taker_buy_base_vol', 'taker_buy_quote_vol', 'ignore'
+    ])
+    df['close'] = df['close'].astype(float)
 
-    if df['EMA_20'].iloc[-1] > df['EMA_50'].iloc[-1]:
-        return 'LONG'
-    elif df['EMA_20'].iloc[-1] < df['EMA_50'].iloc[-1]:
-        return 'SHORT'
+    df['ema_short'] = ta.ema(df['close'], length=9)
+    df['ema_long'] = ta.ema(df['close'], length=21)
+    macd = ta.macd(df['close'], fast=12, slow=26, signal=9)
+    df['macd'] = macd['MACD_12_26_9']
+    df['macds'] = macd['MACDs_12_26_9']
+
+    if df['ema_short'].iloc[-1] > df['ema_long'].iloc[-1] and df['macd'].iloc[-1] > df['macds'].iloc[-1]:
+        return 'long'
+    elif df['ema_short'].iloc[-1] < df['ema_long'].iloc[-1] and df['macd'].iloc[-1] < df['macds'].iloc[-1]:
+        return 'short'
     return None
